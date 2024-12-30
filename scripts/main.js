cc5b614352a1999a877a50b3edd2bc7d46c3fe0e
kourosh <kourosh0903@gmail.com>
2024-12-30 06:46:28 -0800

    removeable style from input

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,48 @@
-
 const emailElm = document.querySelector('.js-email-field');
-const submitBtn = document.querySelector('.js-submit');
-const form = document.querySelector('form');
+const fieldsElm = document.querySelectorAll('.js-fields');
+const radiosElm = document.querySelectorAll('.js-radio');
 
-submitBtn.addEventListener('click', validation);
+const errorField = document.querySelectorAll('.js-error-field');
+const errorEmail = document.querySelector('.js-error-email');
+const errorRadio = document.querySelector('.js-error-radio');
+const errorCheckbox = document.querySelector('.js-error-checkbox');
 
+// remove error style when input focused
+fieldsElm.forEach((elm, i) => {
+  elm.addEventListener('click', () => {
+    elm.classList.remove('red-border');
+    errorField[i].classList.remove('display');
+  });
+});
+
+emailElm.addEventListener('click', () => {
+  emailElm.classList.remove('red-border');
+  errorEmail.classList.remove('display');
+});
+
+radiosElm.forEach(elm => {
+  elm.addEventListener('click', () => {
+    errorRadio.classList.remove('display');
+  });
+});
+
+const checkboxElm = document.querySelector('.js-checkbox');
+checkboxElm.addEventListener('click', () => {
+  errorCheckbox.classList.remove('display');
+});
+
+// add style to radio
+const containersRadio = document.querySelectorAll('.js-radio-cont');
+radiosElm.forEach((elm, i) => {
+  elm.addEventListener('click', () => {
+    containersRadio[i].classList.add('checked');
+  });
+});
+
+// validatin inputs
 function validation() {
   let validate = 0;
-  const fieldsElm = document.querySelectorAll('.js-fields');
-  const errorField = document.querySelectorAll('.js-error-field');
+
   fieldsElm.forEach((elm, i) => {
     if (!elm.value) {
       errorField[i].classList.add('display');
@@ -17,7 +51,6 @@ function validation() {
     }
   });
 
-  const errorEmail = document.querySelector('.js-error-email');
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(emailElm.value)) {
     errorEmail.classList.add('display');
@@ -26,14 +59,12 @@ function validation() {
   }
 
   const selectedRadio = document.querySelector('.js-radio:checked');
-  const errorRadio = document.querySelector('.js-error-radio');
   if (!selectedRadio) {
     errorRadio.classList.add('display');
     validate++;
   }
 
   const checkedInput = document.querySelector('.js-checkbox:checked');
-  const errorCheckbox = document.querySelector('.js-error-checkbox');
   if (!checkedInput) {
     errorCheckbox.classList.add('display');
     validate++;
@@ -41,6 +72,30 @@ function validation() {
 
   return validate;
 }
+
+// remove styles from input
+function removeStyle() {
+  document.querySelectorAll('[class|="js-error"').forEach(elm => {
+    elm.classList.remove('display');
+  });
+
+  document.querySelectorAll('input').forEach(elm => {
+    elm.classList.remove('red-border');
+  });
+
+  document.querySelector('.checked').classList.remove('ckecked');
+}
+
+document.querySelector('.js-submit').addEventListener('click', () => {
+  const validate = validation();
+
+  const form = document.querySelector('form');
+  if (!validate) {
+    removeStyle();
+    form.submit();
+  }
+});
+
 
 
 
